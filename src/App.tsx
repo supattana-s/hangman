a5a7e665d857e999d0cec286867993d82e1b7720
supattana-s <supattana.sue@gmail.com>
2023-02-13 15:45:01 +0700

    done with console.log error occur

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,106 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import HangmanDrawing from "./components/HangmanDrawing";
+import HangmanWord from "./components/HangmanWord";
+import Keyboard from "./components/Keyboard";
+import words from "./wordList.json";
+
+const getWord = () => {
+    return words[Math.floor(Math.random() * words.length)];
+};
 
 function App() {
-    return <h1>HI</h1>;
+    const [wordToGuess, setWordToGuess] = useState(getWord());
+    const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+    const incorrectLetters = guessedLetters.filter(
+        (letter) => !wordToGuess.includes(letter)
+    );
+
+    const isLoser = incorrectLetters.length >= 6;
+    const isWinner = wordToGuess
+        .split("")
+        .every((letter) => guessedLetters.includes(letter));
+
+    const addGuessedLetter = useCallback(
+        (letter: string) => {
+            if (guessedLetters.includes(letter) || isLoser || isWinner) return;
+
+            setGuessedLetters((currentGuessLetters) => [
+                ...currentGuessLetters,
+                letter,
+            ]);
+        },
+        [guessedLetters, isWinner, isLoser]
+    );
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key;
+
+            if (!key.match(/^[a-z]$/)) return;
+
+            e.preventDefault();
+            addGuessedLetter(key);
+        };
+
+        document.addEventListener("keypress", handler);
+
+        return () => {
+            document.removeEventListener("keypress", handler);
+        };
+    }, [guessedLetters]);
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key;
+
+            if (key !== "Enter") return;
+
+            e.preventDefault();
+            setGuessedLetters([]);
+            setWordToGuess(getWord());
+        };
+
+        document.addEventListener("keypress", handler);
+
+        return () => {
+            document.removeEventListener("keypress", handler);
+        };
+    }, []);
+
+    return (
+        <div
+            style={{
+                maxWidth: "800px",
+                display: "flex",
+                flexDirection: "column",
+                gap: "2rem",
+                margin: "0 auto",
+                alignItems: "center",
+            }}
+        >
+            <div style={{ fontSize: "2rem", textAlign: "center" }}>
+                {isWinner && "Winnner ! - Refresh to try again"}
+                {isLoser && "Nice try - Refresh to try again"}
+            </div>
+            <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+            <HangmanWord
+                reveal={isLoser}
+                guessedLetters={guessedLetters}
+                wordToGuess={wordToGuess}
+            />
+            <div style={{ alignSelf: "stretch" }}>
+                <Keyboard
+                    disabled={isWinner || isLoser}
+                    activeLetter={guessedLetters.filter((letter) =>
+                        wordToGuess.includes(letter)
+                    )}
+                    inactiveLetter={incorrectLetters}
+                    addGuessedLetter={addGuessedLetter}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,73 @@
+import styles from "../Keyboard.module.css";
+
+const KEYS = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+];
+
+type KeyboardProps = {
+    activeLetter: string[];
+    inactiveLetter: string[];
+    addGuessedLetter: (letter: string) => void;
+    disabled: boolean;
+};
+
+export function Keyboard({
+    activeLetter,
+    inactiveLetter,
+    addGuessedLetter,
+    disabled = false,
+}: KeyboardProps) {
+    return (
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+                gap: ".5rem",
+            }}
+        >
+            {KEYS.map((key) => {
+                const isActive = activeLetter.includes(key);
+                const isInactive = inactiveLetter.includes(key);
+                return (
+                    <button
+                        onClick={() => addGuessedLetter(key)}
+                        className={`${styles.btn} ${
+                            isActive ? styles.active : ""
+                        } ${isInactive ? styles.inactive : ""}`}
+                        disabled={isActive || isInactive || disabled}
+                        key={key}
+                    >
+                        {key}
+                    </button>
+                );
+            })}
+        </div>
+    );
+}
+
+export default Keyboard;
